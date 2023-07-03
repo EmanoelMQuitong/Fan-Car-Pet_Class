5fdb9b624bbdb978735e314da2465a661c151f06
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-07-03 09:24:39 +0800

    There are troubles in converting 'SLOW' into integer. Setter and Getter are working properly.

---
+++ Fan/Fan-Test-Driver.py
@@ -0,0 +1,60 @@
+from Fan import Fan
+import pyfiglet
+from colored import fg
+
+#pyfiglet format :
+# color = fg('blue')
+#text_print = (f"Text")
+#print(color + pyfiglet.figlet_format(text_print , font= "digital")) 
+color = fg('blue')
+#Colors
+BLUE = '\033[94m'
+DARKCYAN = '\033[36m'
+PURPLE = '\033[95m'
+GREEN = '\033[92m'
+RED = '\033[91m'
+BOLD = '\033[1m'
+END = '\033[0m'
+BORDER = 175*'='
+print(GREEN+BOLD+BORDER+END)
+print(GREEN+BOLD+BORDER+END)
+
+#default:
+f1 = Fan()
+
+print(PURPLE+BOLD+"Fan Default: "+END)
+print('\n')
+f1.show()
+print('\n')
+
+print(GREEN+BOLD+BORDER+END)
+print(GREEN+BOLD+BORDER+END)
+print('\n')
+print(BOLD+BORDER+END)
+print(BOLD+BORDER+END)
+#object 1
+print('\n')
+
+print(BLUE+BOLD+"Fan 1:"+END)
+
+speed = input("How fast do you want your fan?('SLOW','MEDIUM', or 'FAST'):")
+radius = input("How wide is your fan blades?('5.0' to '10.0'): ")
+color = input("Which color do you like?: ")
+power = input("Do you want to turn it on?: ")
+
+f2 = Fan(speed, radius, color, power)
+print('\n')
+print('\n')
+
+print(RED+BOLD+"Fan 1:"+END)
+
+print('\n')
+
+print("Fan speed: ",f2._Fan__speed)
+print("Fan blade Radius: ", f2._Fan__radius)
+print("Fan Color: ", f2._Fan__color)
+print("Power on?: ", f2._Fan__on)
+
+print('\n')
+print(BOLD+BORDER+END)
+print(BOLD+BORDER+END)
--- Fan/Fan.py
+++ Fan/Fan.py
@@ -1,28 +1,47 @@
-class fan:
-    def __init__(self, speed = 1, radius = 5, color = 'blue', on = False):
-        self.__speed = int(speed)
+class Fan:
+    def __init__(self, speed = 'SLOW', radius = 5, color = 'blue', on = False):
+        self.__speed = speed
         self.__radius = float(radius)
         self.__on = bool(on)
-        self.__color = str(color)
+        self.__color = str(color.lower())
 
-    def speed(self, speed):
+    def get_speed(self):
+        return self.__speed
+    
+    def set_speed(self, speed):
         speed = speed.upper()
         if speed == 'SLOW' or speed == 'MEDIUM' or speed == 'FAST':
             if speed == 'SLOW':
-                return speed == 1
+                self.__speed = int(1)
             elif speed == 'MEDIUM':
-                return speed == 2
+                self.__speed = int(2)
             elif speed == 'FAST':
-                return speed == 3
+                self.__speed = int(3)
 
         else:
-            print("Please choose from 'SLOW', 'MEDIUM', and 'FAST' only.")
+            print("Please choose from 'SLOW', 'MEDIUM', and 'FAST' ONLY.")
     
+    def on(self, on):
+        on = on.lower()
+        if on == 'yes' or on == 'no':
+            if on == 'yes':
+                return True
+            if on == 'no':
+                return False
+            
+        else:
+            print("Please choose from 'yes' or 'no' ONLY.")
+
     def show(self):
-        print("Speed: ", self.__speed)
+        print("Speed: ", self.get_speed())
         print("Radius: ", self.__radius)
         print("Is it on?: ", self.__on)
         print("Color: ", self.__color)
     
-f = fan()
-f.show()
+
+f1 = Fan()
+
+f1.show()
+print('\n')
+f1.set_speed('FAST')
+print("Speed: ", f1.get_speed())
