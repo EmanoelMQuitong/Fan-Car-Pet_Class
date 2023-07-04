ddf0ee3b083459dd2eaeea4e4966a75f0f412d95
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-07-04 22:28:38 +0800

    Accessors are successfully added. It works fine. Starting to work for the getters.

---
+++ Pet/Pet.py
@@ -0,0 +1,23 @@
+class Pet:
+    def __init__(self, name, animal_type, age):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
+
+    def set_name(self, name):
+        name = name.upper()
+        self.__name = name
+    
+    def set_animal_type(self, animal_type):
+        animal_type = animal_type.upper()
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        age = int(age)
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def get_animal_type(self):
+        
