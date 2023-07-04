22b23a5be98610c763b5e71fd456fff80af6a4f0
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-07-04 12:40:48 +0800

    Accelerate and brake method has successfully added.

--- Car/Car.py
+++ Car/Car.py
@@ -2,7 +2,7 @@ class Car:
     def __init__(self, year_model, make, speed = 0):
         self.__year_model = year_model
         self.__make = make
-        self.__speed = speed
+        self.__speed = int(speed)
     
     def get_year_model(self):
         return self.__year_model
@@ -13,3 +13,19 @@ class Car:
     def get_speed(self):
         return self.__speed
     
+    def accelerate(self):
+        self.__speed = int(self.__speed) + 5
+        print("Accelerate to: ", self.__speed)
+        return self.__speed
+    
+    def brake(self):
+        self.__speed = int(self.__speed) - 5
+        print("Brake to: ", self.__speed)
+        return self.__speed
+    
+    def show(self):
+        print("year_model: ", self.__year_model)
+        print("make: ", self.__make)
+        print("print: ", self.__speed)
+    
+
